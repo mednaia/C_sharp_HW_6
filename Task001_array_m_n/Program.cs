d49aad54cae91b9bb3230fac0355a72c5bfab7c1
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-06-03 14:56:15 +0300

    Task001_array of real numbers

--- Task001_array_m_n/Program.cs
+++ Task001_array_m_n/Program.cs
@@ -20,7 +20,7 @@ void PrintMatrix(double[,] matrix)
     for(int i=0; i<matrix.GetLength(0); i++)
     {
         for(int j=0; j<matrix.GetLength(1); j++)
-            Console.Write($"{Math.Round(matrix[i,j],2)}.");
+            Console.Write($"{Math.Round(matrix[i,j],2)}  ");
     Console.WriteLine();
     }
 }
